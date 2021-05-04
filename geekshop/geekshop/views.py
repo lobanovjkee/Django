da1926607cab488fbabbe6ca1578797a8a7cd7b9
Sergey Lobanov <lobanovjkee@gmail.com>
2021-05-04 16:57:24 +0300

    lesson_2

--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 
 
 def main(request):
-    return render(request, 'index.html')
+    context = {
+        "title": 'магазин',
+        'topic': 'тренды'
+    }
+    return render(request, 'index.html', context=context)
 
 
 def contacts(request):
-    return render(request, 'contact.html')
+    context = {
+        'title': 'контакты',
+
+    }
+    return render(request, 'contact.html', context=context)
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 
 
 def products(request):
-    return render(request, 'products.html')
+    context = {
+        'title': 'каталог',
+        'links': [
+            {'href': 'mainapp:index', 'name': 'все'},
+            {'href': 'mainapp:index', 'name': 'дом'},
+            {'href': 'mainapp:index', 'name': 'офис'},
+            {'href': 'mainapp:index', 'name': 'модерн'},
+            {'href': 'mainapp:index', 'name': 'классика'}
+        ]}
+
+    return render(request, 'products.html', context=context)
