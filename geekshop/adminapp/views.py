e375107677db10cfd2bb886651576a424b3e55ac
Sergey Lobanov <lobanovjkee@gmail.com>
2021-05-25 18:42:01 +0300

    Заглушка

--- geekshop/adminapp/views.py
+++ geekshop/adminapp/views.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
+from adminapp.forms import ShopUserAdminEditForm
+from authapp.forms import ShopUserRegisterForm
 from authapp.models import ShopUser
 from django.shortcuts import get_object_or_404, render
 from mainapp.models import Product, ProductCategory
@@ -20,15 +24,51 @@ def users(request):
 
 
 def user_create(request):
-    pass
+    title = 'пользователи/создание'
+
+    if request.method == 'POST':
+        user_form = ShopUserRegisterForm(request.POST, request.FILES)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse('admin_staff:users'))
+    else:
+        user_form = ShopUserRegisterForm()
+
+    content = {'title': title, 'update_form': user_form}
+
+    return render(request, 'user_update.html', content)
 
 
 def user_update(request, pk):
-    pass
+    title = 'пользователи/редактирование'
+
+    edit_user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == 'POST':
+        edit_form = ShopUserAdminEditForm(request.POST, request.FILES, instance=edit_user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('admin_staff:user_update', args=[edit_user.pk]))
+    else:
+        edit_form = ShopUserAdminEditForm(instance=edit_user)
+
+    content = {'title': title, 'update_form': edit_form}
+
+    return render(request, 'user_update.html', content)
 
 
 def user_delete(request, pk):
-    pass
+    title = 'пользователи/удаление'
+
+    user = get_object_or_404(ShopUser, pk=pk)
+
+    if request.method == 'POST':
+        user.is_active = False
+        user.save()
+        return HttpResponseRedirect(reverse('admin_staff:users'))
+
+    content = {'title': title, 'user_to_delete': user}
+
+    return render(request, 'user_delete.html', content)
 
 
 def categories(request):
