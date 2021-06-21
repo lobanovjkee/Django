5209c6588d9f1867fd14b41259b1760f21db218c
Sergey Lobanov <lobanovjkee@gmail.com>
2021-06-21 15:10:28 +0300

    ShopUserProfileEditForm

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -4,7 +4,7 @@ from django.core.mail import send_mail
 from django.shortcuts import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from .forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
+from .forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm, ShopUserProfileEditForm
 from .models import ShopUser
 
 
@@ -67,15 +67,21 @@ def edit(request):
 
     if request.method == 'POST':
         edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
-        if edit_form.is_valid():
+        profile_form = ShopUserEditForm(request.POST, instance=request.user.shopuserprofile)
+        if edit_form.is_valid() and profile_form.is_valid():
             edit_form.save()
             return HttpResponseRedirect(reverse('auth:edit'))
     else:
         edit_form = ShopUserEditForm(instance=request.user)
+        profile_form = ShopUserProfileEditForm(instance=request.user.shopuserprofile)
 
-    content = {'title': title, 'edit_form': edit_form}
+    context = {
+        'title': title,
+        'edit_form': edit_form,
+        'profile_form': profile_form,
+    }
 
-    return render(request, 'authapp/edit.html', content)
+    return render(request, 'authapp/edit.html', context)
 
 
 def send_verify_link(user):
