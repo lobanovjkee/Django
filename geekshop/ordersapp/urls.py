7708511eb75d9106a9c3f1198fbb94f3ebe25669
Sergey Lobanov <lobanovjkee@gmail.com>
2021-06-30 15:24:00 +0300

    forms and ListView

--- geekshop/ordersapp/urls.py
+++ geekshop/ordersapp/urls.py
@@ -4,5 +4,10 @@ from django.urls import path
 app_name = 'ordersapp'
 
 urlpatterns = [
-
+    path('', ordersapp.OrderList.as_view(), name='list'),
+    path('create/', ordersapp.CreateOrder.as_view(), name='create'),
+    path('update/<pk>/', ordersapp.UpdateOrder.as_view(), name='update'),
+    path('delete/<pk>/', ordersapp.DeleteOrder.as_view(), name='delete'),
+    path('read/<pk>/', ordersapp.ReadOrder.as_view(), name='read'),
+    path('forming/complete/<pk>', ordersapp.forming_complete, name='forming_complete'),
 ]
--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 
-# Create your views here.
+from ordersapp.models import Order
+
+
+class OrderList(ListView):
+    model = Order
+    template_name = 'order_list.html'
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
+
+class CreateOrder(CreateView):
+    pass
+
+
+class UpdateOrder(UpdateView):
+    pass
+
+
+class DeleteOrder(DeleteView):
+    pass
+
+
+class ReadOrder(DetailView):
+    pass
+
+
+def forming_complete(request, pk):
+    pass
