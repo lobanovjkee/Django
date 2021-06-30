3e7dd08185260c6f63100b5954dac4b707604822
Sergey Lobanov <lobanovjkee@gmail.com>
2021-06-30 16:43:30 +0300

    CreateView, UpdateView

--- geekshop/ordersapp/forms.py
+++ geekshop/ordersapp/forms.py
@@ -10,7 +10,7 @@ class OrderEditForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field_name, field in self.field.items():
+        for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
             field.help_text = ''
 
@@ -18,10 +18,10 @@ class OrderEditForm(forms.ModelForm):
 class OrderItemEditForm(forms.ModelForm):
     class Meta:
         model = OrderItem
-        fields = '__all__'
+        exclude = ()
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name, field in self.field.items():
+        super(OrderItemEditForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
             field.help_text = ''
--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -1,23 +1,99 @@
+from django.db import transaction
+from django.forms import inlineformset_factory
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 
-from ordersapp.models import Order
+from basketapp.models import Basket
+from ordersapp.forms import OrderItemEditForm
+from ordersapp.models import Order, OrderItem
 
 
 class OrderList(ListView):
     model = Order
-    template_name = 'order_list.html'
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
 
 
 class CreateOrder(CreateView):
-    pass
+    model = Order
+    success_url = reverse_lazy('order:list')
+    fields = []
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemEditForm, extra=1)
+        if self.request.POST:
+            formset = OrderFormSet(self.request.POST)
+        else:
+            basket_items = Basket.objects.filter(user=self.request.user)
+            if basket_items.exists():
+                OrderFormSet = inlineformset_factory(
+                    Order,
+                    OrderItem,
+                    form=OrderItemEditForm,
+                    extra=basket_items.count(),
+                )
+                formset = OrderFormSet()
+                for num, form in enumerate(formset.forms):
+                    form.initial['product'] = basket_items[num].product
+                    form.initial['quantity'] = basket_items[num].quantity
+            else:
+                formset = OrderFormSet()
+
+        data['orderitems'] = formset
+
+        return data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        orderitems = context['orderitems']
+
+        with transaction.atomic():
+            form.instance.user = self.request.user
+            self.object = form.save()
+            if orderitems.is_valid():
+                orderitems.instance = self.object
+                orderitems.save()
+
+        if self.object.get_total_cost() == 0:
+            self.object.delete()
+
+        return super().form_valid(form)
 
 
 class UpdateOrder(UpdateView):
-    pass
+    model = Order
+    success_url = reverse_lazy('order:list')
+    fields = []
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemEditForm, extra=1)
+        if self.request.POST:
+            formset = OrderFormSet(self.request.POST, instance=self.object)
+        else:
+            formset = OrderFormSet(instance=self.object)
+
+        data['orderitems'] = formset
+
+        return data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        orderitems = context['orderitems']
+
+        with transaction.atomic():
+            self.object = form.save()
+            if orderitems.is_valid():
+                orderitems.instance = self.object
+                orderitems.save()
+
+        if self.object.get_total_cost() == 0:
+            self.object.delete()
+
+        return super().form_valid(form)
 
 
 class DeleteOrder(DeleteView):
