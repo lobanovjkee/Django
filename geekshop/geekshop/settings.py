2140930098fb13e28a8d8ac44ba6986b01ab716a
Sergey Lobanov <lobanovjkee@gmail.com>
2021-07-11 18:10:17 +0300

    settings

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -87,11 +87,18 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'CHARSET': 'utf-8',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'CHARSET': 'utf-8',
+        'NAME': 'geekshop',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres'
     }
 }
 
